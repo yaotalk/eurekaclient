7febb8dfbf4fe5d47174d1f05486315ee98e3f37
yaotalk <2362198826@qq.com>
2016-12-30 01:21:06 +0800

    # client for mac

---
+++ src/main/java/com/eruekaClientApplication.java
@@ -0,0 +1,12 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class eruekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(eruekaClientApplication.class, args);
+	}
+}
